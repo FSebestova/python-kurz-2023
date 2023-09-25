99cff15ad40b4aaf027f6bff8e63606f3f6f9070
FSebestova <f.sebestova@seznam.cz>
2023-09-25 18:42:31 +0200

    Hotový základní úkol plus část bonusu

---
+++ ukol-01.py
@@ -0,0 +1,10 @@
+jmeno = str(input("Zadej jméno:"))
+domena = "@czechitas.cz"
+email = str(jmeno + domena)
+print(f"Váš email je: {email}")
+
+#BONUS
+
+jmeno_a_prijmeni = str(input("Zadej jméno: "))
+print(jmeno_a_prijmeni.upper()) #velká
+print(jmeno_a_prijmeni.lower()) #malá
