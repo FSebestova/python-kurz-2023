32b58f9c6eab289c2cb5130e4cb8b7c668a13fb0
FSebestova <f.sebestova@seznam.cz>
2023-10-09 16:09:06 +0200

    zmena slozky

---
+++ Ukoly/ukol-02.py
@@ -0,0 +1,27 @@
+sklad = {
+  "1N4148": 250,
+  "BAV21": 54,
+  "KC147": 147,
+  "2N7002": 97,
+  "BC547C": 10
+}
+
+
+soucastka = input("Kód součástky: ")
+
+if soucastka in sklad:
+    mnozstvi = int(input("Zadejte požadované množství:"))
+    if mnozstvi <= sklad[soucastka]:   #vypíše vypíše pouze hodnotu
+      print(f"Požadované množství je skladem")
+      nove_mnozstvi = sklad[soucastka] - mnozstvi
+      a = {soucastka: nove_mnozstvi} #nově definovaná upravená hodnota a klíč ve slovníku 
+      sklad.update(a)
+      print(f"Nově skladem: {sklad[soucastka]}")
+    else:
+        sklad.pop(soucastka)  #odebrání hodnoty ze slovníku
+        print("Lze prodat jen omezené množství")
+
+else:
+    print("Součástka není skladem")
+    
+    
