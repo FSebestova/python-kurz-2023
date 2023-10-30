4d95f5a163de3830c2ad7ae40b69196ccb02fb7c
FSebestova <f.sebestova@seznam.cz>
2023-10-30 17:07:46 +0100

    bonus done

--- Ukoly/ukol-05.py
+++ Ukoly/ukol-05.py
@@ -20,3 +20,15 @@ print(ranni_teploty)
 print(nocni_teploty)
 print(poledne_noc)
 
+teploty2 = [
+    ["pondělí", 2.1, 5.2, 6.1, -0.1],
+    ["úterý", 2.2, 4.8, 5.6, -1.0],
+    ["středa", 3.3, 6.5, 5.9, 1.2],
+    ["čtvrtek", 2.9, 5.6, 6.0, 0.0],
+    ["pátek", 2.0, 4.6, 5.5, -1.2],
+    ["sobota", 1.0, 3.2, 2.1, -2.0],
+    ["neděle", 0.4, 2.7, 1.3, -2.8]
+]
+
+den_teplota ={den[0]:statistics.mean(den[1:]) for den in teploty2}
+print(den_teplota)
